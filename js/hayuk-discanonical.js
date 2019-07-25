c42d463003d0f846eb857dc68a009122fd7c6b0f
Ramadhan Hadiatma <nandaaprilia605@gmail.com>
2019-07-25 14:05:43 +0700

    Add files via upload

---
+++ js/hayuk-discanonical.js
@@ -0,0 +1,7 @@
+var disqus_shortname = &#39;https-www-hayukjalan-com&#39;;
+var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+if (!disqus_blogger_current_url.length) {
+disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+}
+var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
