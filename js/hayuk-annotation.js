ccc6ed98a360e759b413af7a509ee42b64a03bac
Ramadhan Hadiatma <nandaaprilia605@gmail.com>
2019-07-25 13:34:48 +0700

    Add files via upload

---
+++ js/hayuk-annotation.js
@@ -0,0 +1,2 @@
+google.load(&quot;annotations&quot;, &quot;1&quot;, {&quot;locale&quot;: &quot;<data:top.languageCode/>&quot;});
+function initialize() {google.annotations.setApplicationId(<data:top.blogspotReviews/>);google.annotations.createAll();google.annotations.fetch();}google.setOnLoadCallback(initialize);
