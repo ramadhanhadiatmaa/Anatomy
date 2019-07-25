24c44f9eb7baa9cb9173a52db8deabf0c48f5b68
Ramadhan Hadiatma <nandaaprilia605@gmail.com>
2019-07-25 13:39:52 +0700

    Add files via upload

---
+++ js/hayuk-jquery.js
@@ -0,0 +1,6 @@
+//<![CDATA[
+if (typeof(jQuery) == 'undefined') {
+//output the script (load it from google api)
+ document.write("<scr" + "ipt type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></scr" + "ipt>");
+}
+//]]>
