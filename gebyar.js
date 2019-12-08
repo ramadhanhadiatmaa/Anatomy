9773c5f2e24c08f1df786b3737f765af7d40a73c
Ramadhan Hadiatma <nandaaprilia605@gmail.com>
2019-12-08 09:57:33 +0700

    Add files via upload

--- gebyar.js
+++ gebyar.js
@@ -7,26 +7,65 @@ function storeData(){
 
 	var hp = document.getElementById("hp").value;
 
+	var modal = document.getElementById("myModal1");
+	var modal2 = document.getElementById("myModal2");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("submit");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
 	db.collection("null").add({
 
 	})
 	.then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
-        
- 
 
 
 	if (akun === ""){
-		alert("Masukan akun sosmed");
+		//alert("Masukan akun sosmed");
+		
+  		modal.style.display = "block";
+
+  		span.onclick = function() {
+		  modal.style.display = "none";
+		}
+
+		window.onclick = function(event) {
+		  if (event.target == modal) {
+		    modal.style.display = "none";
+		  }
+		}
+
+		return false;
+	}else{
+	
+	}
+
+
+	if (hp === ""){
+		modal2.style.display = "block";
+
+  		span.onclick = function() {
+		  modal2.style.display = "none";
+		}
+
+		window.onclick = function(event) {
+		  if (event.target == modal2) {
+		   modal2.style.display = "none";
+		  }
+		}
+
 		return false;
 	}else{
 
 	}
 
-	if (hp === "") {
-		alert("Masukan nomor hp");
-		return false;
-	}
+
+
+	// When the user clicks anywhere outside of the modal, close it
+	
 	
 	db.collection("daftar").doc(docRef.id).set({
           id: docRef.id,
